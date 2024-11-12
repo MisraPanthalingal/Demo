121a69e132fd211f40f0c8436832e637203aecd5
Misra Panthalingal <106969756+MisraPanthalingal@users.noreply.github.com>
2024-11-12 15:10:38 +0530

    port number changed
    port number changed to 8000

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
-port = process.env.PORT || 7000;
+port = process.env.PORT || 8000;
 console.log(port)
 
 
